2c715191e5560eb4a5adc7e9f5e8cf6c0e4d6bce
fritnes <fritnes@maxitech.com.ua>
2022-07-14 14:25:14 +0300

    Change apt.sh

--- files/apt.sh
+++ files/apt.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 #
+# Description: Expose metrics from apt updates.
+#
+# Author: Ben Kochie <superq@gmail.com>
 
-apt update -y > /dev/null 2>&1
-
-echo '# HELP apt_total_upgrades_pending total update.'
-echo '# TYPE apt_total_upgrades_pending gauge'
-total_update="$(( $(apt list --upgradable 2>/dev/null | grep upgradable | wc -l) - 1 ))" 
-echo "apt_total_upgrades_pending{} "$total_update
-
-echo '# HELP apt_upgrades_pending list of upgeades package.'
-echo '# TYPE apt_upgrades_pending gauge'
-for packege in $(apt list --upgradable 2>/dev/null | grep upgradable | cut -d '/' -f 1 )
-do
-        echo 'apt_upgrades_pending{package='"$packege"'} 1'
-done
-
-
-echo '# HELP apt_autoremove_pending Apt package pending autoremove.'
-echo '# TYPE apt_autoremove_pending gauge'
+upgrades="$(/usr/bin/apt-get --just-print dist-upgrade \
+  | /usr/bin/awk -F'[()]' \
+      '/^Inst/ { sub("^[^ ]+ ", "", $2); gsub(" ","",$2);
+                 sub("\\[", " ", $2); sub("\\]", "", $2); print $2 }' \
+  | /usr/bin/sort \
+  | /usr/bin/uniq -c \
+  | awk '{ gsub(/\\\\/, "\\\\", $2); gsub(/"/, "\\\"", $2);
+           gsub(/\[/, "", $3); gsub(/\]/, "", $3);
+           print "apt_upgrades_pending{origin=\"" $2 "\",arch=\"" $NF "\"} " $1}'
+)"
 autoremove="$(/usr/bin/apt-get --just-print autoremove \
   | /usr/bin/awk '/^Remv/{a++}END{printf "apt_autoremove_pending %d", a}'
 )"
+echo '# HELP apt_upgrades_pending Apt package pending updates by origin.'
+echo '# TYPE apt_upgrades_pending gauge'
+if [[ -n "${upgrades}" ]] ; then
+  echo "${upgrades}"
+else
+  echo 'apt_upgrades_pending{origin="",arch=""} 0'
+fi
+echo '# HELP apt_autoremove_pending Apt package pending autoremove.'
+echo '# TYPE apt_autoremove_pending gauge'
 echo "${autoremove}"
-
-
 echo '# HELP node_reboot_required Node reboot is required for software updates.'
 echo '# TYPE node_reboot_required gauge'
-
 if [[ -f '/run/reboot-required' ]] ; then
   echo 'node_reboot_required 1'
 else
